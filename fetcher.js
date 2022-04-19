763b8364a7e1a223a1360b57eef7d3f149e24e65
Winston Limanto <wmlimanto@gmail.com>
2022-04-18 20:00:05 -0700

    added edge case if URL is invalid

--- fetcher.js
+++ fetcher.js
@@ -8,15 +8,29 @@ const fs = require('fs');
 const args = process.argv.slice(2);
 const url = args[0];
 const filePath = args[1];
+// readline module
+const readline = require('readline');
+const path = require('path');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 request(url, (error, response, body) => {
-  console.log('statusCode: ', response);
+  console.log('StatusCode: ', response && response.statusCode);
+
+  // edge case: if URL is invalid or non-200
+  if (response.statusCode !== 200) {
+    console.log('The URL specified is invalid - please try again!');
+    rl.close();
+  } else {
 
   fs.writeFile(filePath, body, error => {
     if (error) {
-      console.log('error: ', error);
+      console.log('Error: ', error);
       return
     }
   })
-  console.log(`downloaded and saved ${body.length} bytes to ${filePath}`)
+  console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`)
+}
 });
